32ce46ed18e40f42727911ff66a3c1780c2eaeb6
akisarou <akisarou90@gmail.com>
2020-06-01 12:51:46 +0300

    Removed check if position is the same with this.currentWorkingPosition on moveToElementByIndex

---
+++ lib/constants.d.ts
@@ -0,0 +1,16 @@
+export declare const kHundredPercent = "100%";
+export declare const kTwoHundredPercent = "200%";
+export declare const kZero = "0";
+export declare const kPlus = "+";
+export declare const kMinus = "-";
+export declare const kPercentage = "%";
+export declare const kPixel = "px";
+export declare const kDot = ".";
+export declare const kTranslateX = "translateX";
+export declare const kAnimatedClass = "Animated";
+export declare const kTransitionEnd = "transitionend";
+export declare const kTouchStart = "touchstart";
+export declare const kTouchMove = "touchmove";
+export declare const kTouchEnd = "touchend";
+export declare const kSwipporDataset = "data-swippordatasetindex";
+//# sourceMappingURL=constants.d.ts.map
